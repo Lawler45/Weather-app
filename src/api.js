d291b12e9cf34cc90cb0d33ed7d8974b1394a5f6
Lewis Lawler <lewislawler98@gmail.com>
2023-12-06 15:31:01 +0000

    adds fully functioning search bar

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2ca51a0f1emsh25774b83bf020fbp1d4dd5jsnfe363256f994",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
