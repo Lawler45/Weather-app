240da78c87ddd5ea653952edd2a837da7c48f402
Lewis Lawler <lewislawler98@gmail.com>
2023-12-06 16:01:45 +0000

    adds basic styling to the current weather

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,13 +1,16 @@
 import "./current-weather.css";
+import "../current-weather/current-weather.css";
 
 const CurrentWeather = () => {
   return (
     <div className="weather">
-        <div className="top">
-            <p className="city">Manchester</p>
-            <p className="weather-description">Sunny</p>
+      <div className="top">
+        <div>
+          <p className="city">Manchester</p>
+          <p className="weather-description">Sunny</p>
         </div>
         <img alt="weather icon" className="weather-icon" src="icons/01d.png" />
+      </div>
     </div>
   );
 };
