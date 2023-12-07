a6d748923eff7c488661fa1b72957e46b40a46c4
Lewis Lawler <lewislawler98@gmail.com>
2023-12-07 12:35:40 +0000

    adds real data from api

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "75c5245b2420352de66275c600f8693c"
