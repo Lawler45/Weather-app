09c63d262952a17c3be841971ad0cb49d37e86e1
Lewis Lawler <lewislawler98@gmail.com>
2024-01-04 14:29:00 +0000

    adds template for forecast data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 import { useState } from "react";
+import Forecast from "./components/forecast/forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -16,7 +17,7 @@ function App() {
     );
 
     const forecastFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
 
     Promise.all([currentWeatherFetch, forecastFetch])
@@ -30,10 +31,13 @@ function App() {
       .catch((error) => console.log(error));
   };
 
+  console.log(forecast, 'forecast')
+
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,27 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemPanel,
+  AccordionItemHeading,
+  AccordionItemButton,
+} from "react-accessible-accordion";
+
+const Forecast = ({ data }) => {
+  return (
+    <>
+      <label className="title">Daily</label>
+      <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+          <AccordionItem key={idx}>
+            <AccordionItemHeading>
+              <AccordionItemButton>Hello</AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel></AccordionItemPanel>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Forecast;
